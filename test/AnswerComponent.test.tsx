f9135b4fbb61339e39acb7dd854dc282e1b94a71
AdrienDubostIsitech <adrien.dubost@ecole-isitech.fr>
2023-04-27 10:53:00 +0200

    [TEST] vitest-implementation unit test of Answer and Question component

--- test/AnswerComponent.test.tsx
+++ test/AnswerComponent.test.tsx
@@ -1,23 +1,19 @@
 import { AnswerComponent } from '../src/components/AnswerComponents'
-import { expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
-import "@testing-library/dom"
 import React from 'react';
 
 describe('reverse', () => {
 
   test("return array with reversed string if array contains multiple elements", () => {
-    const testvalues = ["test1","test2","test3","test4"]; 
-    render(<AnswerComponent answer={testvalues} onSelect={(answer: string) => undefined}/>); 
-    const value1 = screen.getByText(/test1/i); 
-    const value2 = screen.getByText(/test2/i); 
-    const value3 = screen.getByText(/test3/i); 
-    const value4 = screen.getByText(/test4/i); 
-    expect(value1).toBe;
-    expect(value2).toBe;
-    expect(value3).toBe;
-    expect(value4).toBe;
-    // const reversed = AnswerComponent({answer:["test1","test2","test3","test4"], onSelect:(answer: string) => undefined})
-    // expect(reversed).toEqual({answer:["test1","test2","test3","test4"], onSelect:(answer: string) => undefined})
+    const testvalues = ["test1", "test2", "test3", "test4"];
+    render(<AnswerComponent answer={testvalues} onSelect={(answer: string) => undefined} />);
+    const value1 = screen.getByText(testvalues[0]);
+    const value2 = screen.getByText(testvalues[1]);
+    const value3 = screen.getByText(testvalues[2]);
+    const value4 = screen.getByText(testvalues[3]);
+    console.log(value1);
+    console.log(value2);
+    console.log(value3);
+    console.log(value4);
   });
 });
--- test/QuestionComponent.test.tsx
+++ test/QuestionComponent.test.tsx
@@ -1,25 +1,15 @@
-import { it, describe, expect, assert, vi} from "vitest";
-import { setup } from "./utils";
+import { describe} from "vitest";
+import { render, screen } from '@testing-library/react'
 import { QuestionComponent } from "../src/components/QuestionComponent";
-import ReactDOM from "react-dom/client";
-import { act } from "@testing-library/react";
 import React from "react";
 
 
 
 describe("Counter", () => {
-  it("should increment when clicking on increment button", async () => {
-
-    const container = document.createElement("div");
-    document.body.append(container);
-
-    act(() => {
-
-      const root = ReactDOM.createRoot(container);
-      root.render(<QuestionComponent name="France"/>);
-    });
-
-    console.log(document.body.innerHTML);
-
-  });
+  test("return the name of the country in props", () => {
+    const testvalue = "JeSuisUnPays"; 
+    render(<QuestionComponent name={testvalue}/>); 
+    const valueName = screen.getByText("Quel est la capitale de ce pays : " + testvalue + " ?"); 
+    console.log(valueName); 
+  })
 });

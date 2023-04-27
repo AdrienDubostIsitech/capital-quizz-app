a0322d67f8490b61645d8571033000fe0e318b41
JulienHanon <61320794+JulienHanon@users.noreply.github.com>
2023-04-27 16:47:34 +0200

    style: 💄 add style for dark mode (#12)

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <h1 className='font-bold text-5xl text-center text-indigo-700'>LE QUIZ DES CAPITALES</h1>
-      <div className="bg-white p-12 rounded-lg shadow-lg w-full mt-8">
+      <div className="bg-white dark:bg-slate-700 p-12 rounded-lg shadow-lg w-full mt-8">
         <QuizManagerComponents/>
       </div>
     </>
